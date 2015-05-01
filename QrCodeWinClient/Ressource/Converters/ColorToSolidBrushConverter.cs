856c33d419270d25000441e2c753a4a5f4c68202
Daniel <dhcgn@posteo.ch>
2015-05-01 12:47:45 +0200

    Added color picker

---
+++ QrCodeWinClient/Ressource/Converters/ColorToSolidBrushConverter.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace QrCodeWinClient
+{
+    [ValueConversion(typeof(SolidBrush), typeof(System.Windows.Media.Color))]
+    public class ColorToSolidBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var solidBrush = value as SolidBrush;
+
+            if (solidBrush == null) return null;
+
+            var color = solidBrush.Color;
+            return System.Windows.Media.Color.FromArgb(color.A, color.R, color.G, color.B);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || !(value is System.Windows.Media.Color)) return null;
+            var color = (System.Windows.Media.Color) value;
+
+            return new SolidBrush(System.Drawing.Color.FromArgb(color.A, color.R, color.G, color.B));
+        }
+    }
+}
